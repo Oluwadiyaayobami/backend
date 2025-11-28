a531867f02cd11f611de432f10eb775956d93de1
oluwadiyaayobami <eniolaabimbola09@gmail.com>
2025-11-28 10:51:13 +0100

    i learnt node file system and the uses how we can use it to control file structur

---
+++ 5file-system/index.js
@@ -0,0 +1,25 @@
+const fs = require('fs')
+const path = require('path')
+
+
+const filepath  =  path.join(__dirname,'filesystem')
+
+fs.mkdir(filepath,(error) => {
+    if(error){
+        console.log('error cerating file ')
+    }
+    else{
+        console.log('file cerated succesfuly ')
+    }
+})
+
+const folderfile = path.join(filepath,'server.js')
+
+fs.writeFile(folderfile,"console.log('welcome')" ,(error)=>{
+    if(error){
+        console.log('file was not added ')
+    }
+    else{
+        console.log('file cerated sucessfuly ')
+    }
+})
